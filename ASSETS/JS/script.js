c1542b6f4713da7fa43c93d443726173b68b6b2b
risdlcrz <155805849+risdlcrz@users.noreply.github.com>
2024-03-02 16:18:39 +0800

    Add files via upload

---
+++ ASSETS/JS/script.js
@@ -0,0 +1,32 @@
+var audio = document.getElementById('audio-background');
+
+function toggleSound() {
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+    }
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function hexToRGBA(hex, alpha) {
+    var r = parseInt(hex.slice(1, 3), 16),
+        g = parseInt(hex.slice(3, 5), 16),
+        b = parseInt(hex.slice(5, 7), 16);
+
+    if (alpha) {
+        return "rgba(" + r + ", " + g + ", " + b + ", " + alpha + ")";
+    } else {
+        return "rgb(" + r + ", " + g + ", " + b + ")";
+    }
+}
+
+setInterval(changeColor, 1000);
