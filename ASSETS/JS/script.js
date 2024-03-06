bc1f8c52d7d3662006ae958b6e22da7088bd1019
risdlcrz <155805849+risdlcrz@users.noreply.github.com>
2024-03-06 20:42:20 +0800

    Update script.js

--- ASSETS/JS/script.js
+++ ASSETS/JS/script.js
@@ -5,22 +5,22 @@ fetch('./projects.json')
         projects.forEach(project => {
             const item = document.createElement('div');
             item.className = 'category-item';
-            item.dataset.href = project.href;
+            item.dataset.href = project.href; // Change this line to use 'href'
 
             const defaultImg = document.createElement('img');
             defaultImg.src = './ASSETS/screen.png';
-            defaultImg.alt = project.name;
+            defaultImg.alt = project.name; // Change this line to use 'name'
             item.appendChild(defaultImg);
 
             const hoverImg = document.createElement('img');
             hoverImg.src = project.thumbnail;
-            hoverImg.alt = `${project.name} Hover`;
+            hoverImg.alt = `${project.name} Hover`; // Change this line to use 'name'
             hoverImg.className = 'category-image-hover';
             item.appendChild(hoverImg);
 
             const title = document.createElement('div');
             title.className = 'category-title';
-            title.textContent = project.name;
+            title.textContent = project.name; // Change this line to use 'name'
             item.appendChild(title);
 
             // Add a click event listener to the project div
